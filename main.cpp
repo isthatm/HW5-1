3527aceb66fb4859583d6be5db51c85eb5fad320
isthatm <91708157+isthatm@users.noreply.github.com>
2023-01-04 20:13:37 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,87 +10,107 @@ Thread carThread;
 Thread measureThread;
 EventQueue car_queue;
 EventQueue measure_queue;
+
+//Car
 PwmIn servo0_f(D12), servo1_f(D11); // servo0 - left ; servo1 - right
 PwmOut servo0_c(D9), servo1_c(D10); 
-
-DigitalInOut pin8(D8);
-
 BBCar car(servo0_c, servo0_f, servo1_c, servo1_f, servo_ticker, servo_feedback_ticker);
 
-float width;
-float nonObjectTable [2];
-float objectTable[2];
-int i1 = 0; int i2 = 0;
+//Laser ping
+DigitalInOut pin8(D8);
+parallax_ping  ping1(pin8);
 
-//distance to objects 20cm
-// distance between two objects
+// QTI sensors
+BusInOut qti_pin(D4,D5,D6,D7);
+parallax_qti qti1(qti_pin);
+int pattern;
+int currentAngle;
 
-void obsMeasure(){
-    float distance;
-    parallax_ping  ping1(pin8);
-    while(1) {
-        
-        distance = (float)ping1;
-        printf("distance = %f\n", distance);
-        if(distance<25){
-            objectTable[i1] = distance;
-            if (i1 == 0){
-                i2 = 1;
-            }
-            else if(i1 == 1){
-                i1 = 2; // edege of the object is found
-            }
-        } 
-        else {
-            nonObjectTable[i2] = distance;
-            if (i2 == 1){ 
-                // impying that the last number in objectTable is the inner edge
-                // increase i to store the distance of the 2nd objec innter edge
-                i1=1;
-            }
-        }
-        if (i1 == 2){
-            break;
-        }
-        ThisThread::sleep_for(1s); 
-   }
-   
-   width = sqrt((objectTable[0]*objectTable[0] + objectTable[1]*objectTable[1] )/2 - 19*19);
-   printf("a = %f, b = %f, w = %f \n", objectTable[0], objectTable[1], width);                                
-   
-   
+const float PI = 3.13159;
+const float R = 5.1; //distance between wheels -> radius of rotaion
+const float rotP = 32.04424507;
+float obj[2], w[2];
+int i = 0;
+int rotatingSpeed = 45;
+
+int obsCaution = 1;
+
+
+void widthCalculation (double d, double currentWheelAngle, double initWheelAngle ){
+    float wheelAngle, rotAngle, p, theta;
+    wheelAngle =  abs(currentWheelAngle) - abs(initWheelAngle);
+    p = 6.5*PI*( abs(wheelAngle) / 360 );
+    theta = (p / rotP)*360; //
+    //alpha1 = 90 - theta1;
+    rotAngle = (theta*PI) / 180; // deg to rad
+    w[i] = sin(rotAngle)*d;
+    i++;
 }
 
-void rotate2Measure (){
-    float dist = 7;
-    float err = 0.7;
-    car.rotateCertainDistance(-dist);
-    while (car.checkRotateDistance(err)){
-        ThisThread::sleep_for(100ms);
+void obsMeasure1(){
+    double d1, d2, obsMeasureAngle0;
+    obsMeasureAngle0 = car.servo1.angle;
+    while(1) {
+        d1 = (float)ping1;
+        if (d1 > 20){
+            printf("d1: %f\n", d1);
+            car.rotate(rotatingSpeed);
+        }
+        else{
+            car.stop();
+            break;
+        }
+        ThisThread::sleep_for(10ms);
     }
-    car.stop();
-    measure_queue.call(&obsMeasure);
-    ThisThread::sleep_for(2s);
-
-    car.rotateCertainDistance(1.7*dist);
-    while (car.checkRotateDistance(1.7*err)){
-        ThisThread::sleep_for(100ms);
-    }
-    car.stop();
-    ThisThread::sleep_for(2s);
-
-    car.rotateCertainDistance(-dist);
-    while (car.checkRotateDistance(err)){
-        ThisThread::sleep_for(100ms);
-    }
-    car.stop();
-    ThisThread::sleep_for(2s);
+    currentAngle = car.servo1.angle;
+    widthCalculation(d1, currentAngle, obsMeasureAngle0);
+    printf("Distance to object 1: %f , w1: %f\n", d1, w[0]);
     
+    // 2nd object
+    while(1) {
+        pattern = (int)qti1;
+        if (pattern == 0b0110){
+            car.stop();
+            ThisThread::sleep_for(1s);
+            obsMeasureAngle0 = car.servo1.angle; 
+            break;
+        }
+        else {
+            car.rotate(-rotatingSpeed);
+        }
+    }
+    while(1){
+        d2 = (float)ping1;
+        if (d2 > 20){
+            printf("d2: %f\n", d2);
+            car.rotate(-rotatingSpeed);
+        }
+        else{
+            car.stop();
+            break;
+        }
+        ThisThread::sleep_for(10ms);
+    }
+    obsCaution = 1;
     
+    widthCalculation(d2 , car.servo1.angle, obsMeasureAngle0);
+    printf("Distance to object 2: %f , w2: %f\n", d2, w[1]);
+    printf("Distance between 2 objects: %f\n", w[0] + w[1]);
+    
+    while(1){
+       pattern = (int)qti1;
+       if (pattern == 0b0110){
+           car.stop();
+           break;
+       }
+       else{
+           car.rotate(rotatingSpeed);
+       }
+   }
 }
 
 int main() {
     carThread.start(callback(&car_queue, &EventQueue::dispatch_forever));
-    measureThread.start(callback(&measure_queue, &EventQueue::dispatch_forever));
-    car_queue.call(&rotate2Measure);
+    car_queue.call(&obsMeasure1); 
 }
+
